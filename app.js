3b565a17d600f0f995181fb464822fd2399f4762
Sagar Patidar <sagarpatidar2002@gmail.com>
2022-09-22 13:10:07 +0530

    final

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let temp = 30;
 let icon;
 let link, temp_f, dt, cloudy, humidity, pressure, wind, main;
-let h_temp = [], h_pop = [], h_time = [],h_link=[];
+let h_temp = [], h_pop = [], h_time = [], h_link = [];
 app.get("/", function (req, res) {
   let url = "https://api.openweathermap.org/data/2.5/weather?q=bhopal&appid=ce3bfcd5c9db9c2e58557b2c6082c035&units=metric"
   https.get(url, function (responce) {
@@ -22,13 +22,13 @@ app.get("/", function (req, res) {
       temp_f = parseInt(wdata.main.feels_like);
       icon = wdata.weather[0].icon;
       link = "/img/" + icon + "@2x.png"
-      dt = givedate(Number(wdata.dt+"000"));
+      dt = givedate(Number(wdata.dt + "000"));
       main = wdata.weather[0].main;
       cloudy = wdata.clouds.all + "%";
       humidity = wdata.main.humidity + "%";
       wind = parseInt(((wdata.wind.speed) * 18) / 5) + "Km/h";
       pressure = wdata.main.pressure + "hPa";
-      
+
     })
   });
   let url2 = "https://api.openweathermap.org/data/2.5/forecast?q=bhopal&appid=ce3bfcd5c9db9c2e58557b2c6082c035&units=metric"
@@ -40,8 +40,8 @@ app.get("/", function (req, res) {
       await datas.push(data);
     })
     responce.on("end", () => {
-      let data   = Buffer.concat(datas);
-      fdata =  JSON.parse(data);
+      let data = Buffer.concat(datas);
+      fdata = JSON.parse(data);
       for (let i = 0; i < 6; i++) {
         let sec = parseInt((fdata.list[i].dt + "000"));
         let h_dt = new Date(sec);
@@ -57,9 +57,9 @@ app.get("/", function (req, res) {
         let t = parseInt(fdata.list[i].main.temp);
         h_temp[i] = t;
         let l = fdata.list[i].weather[0].icon
-        h_link[i] =  "/img/"+ l + "@2x.png";
+        h_link[i] = "/img/" + l + "@2x.png";
       }
-      
+
       res.render('index', {
         temp: temp, temp_f: temp_f, link: link, city: "Bhopal", invalid: "", date: dt, main: main,
         pressure: pressure, wind: wind, cloudy: cloudy, humidity: humidity,
@@ -73,6 +73,9 @@ app.get("/", function (req, res) {
   })
 
 })
+
+
+
 let prev_city, city = "Bhopal", invalid_chack = 0, B_city;
 app.post("/", function (req, res) {
   prev_city = city;
@@ -108,7 +111,7 @@ app.post("/", function (req, res) {
         icon = wdata.weather[0].icon;
         link = "/img/" + icon + "@2x.png"
         main = wdata.weather[0].main;
-        dt = givedate(Number(wdata.dt+"000"));
+        dt = givedate(Number(wdata.dt + "000"));
         cloudy = wdata.clouds.all + "%";
         humidity = wdata.main.humidity + "%";
         wind = parseInt(((wdata.wind.speed) * 18) / 5) + "Km/h";
@@ -119,11 +122,11 @@ app.post("/", function (req, res) {
       let url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ce3bfcd5c9db9c2e58557b2c6082c035&units=metric"
       https.get(url2, function (responce) {
 
-        var datas = [];
-        responce.on("data", function (data) {
-          datas.push(data);
+        var datas=[];
+        responce.on("data", async function (data) {
+          await datas.push(data);
         })
-        responce.on("end", () => {
+        responce.on("end", async () => {
           const fdata = JSON.parse(datas);
           for (let i = 0; i < 6; i++) {
             let sec = parseInt((fdata.list[i].dt + "000"));
@@ -140,7 +143,7 @@ app.post("/", function (req, res) {
             let t = parseInt(fdata.list[i].main.temp);
             h_temp[i] = t;
             let l = fdata.list[i].weather[0].icon
-            h_link[i] =  "/img/"+ l + "@2x.png";
+            h_link[i] = "/img/" + l + "@2x.png";
           }
           res.render('index', {
             temp: temp, temp_f: temp_f, link: link, city: city, invalid: "", date: dt, main: main,
@@ -158,9 +161,15 @@ app.post("/", function (req, res) {
   });
 
 })
-let ofset = 330*60*1000;
+
+
+
+
+
+
+let ofset = 330 * 60 * 1000;
 function givedate(sec) {
-  let dt = new Date(sec+ofset);
+  let dt = new Date(sec);
   let h = dt.getHours();
   if (h > 12) {
     h = h - 12;
@@ -241,6 +250,12 @@ function givedate(sec) {
   return `${h}:${m} ${day},${dat} ${month} ${year}`
 }
 
-app.listen(process.env.PORT||3000, () => {
+app.use((err,req,res,next)=>{
+res.status(err.status||500)
+res.redirect("/")
+console.log("Error")
+})
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Port 3000");
 })
